17e595931d8dd1ca222ddbe0926dd02ba2468e25
Dan Chenoweth <dan.chenoweth@givedirectly.org>
2023-02-20 09:32:53 -0500

    introduce defaults

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/dcheno/prompt
 
 go 1.19
 
-require github.com/dcheno/scripter v0.1.0
+require github.com/dcheno/scripter v0.1.2
--- prompt_test.go
+++ prompt_test.go
@@ -1,9 +1,12 @@
 package prompt_test
 
 import (
+	"fmt"
+	"strings"
+	"testing"
+
 	"github.com/dcheno/prompt"
 	"github.com/dcheno/scripter"
-	"testing"
 )
 
 func TestPromptReturnsValidAnswer_ShortCode(t *testing.T) {
@@ -20,7 +23,7 @@ func TestPromptReturnsValidAnswer_ShortCode(t *testing.T) {
 
 	script := scripter.NewScript(
 		t,
-		scripter.Expect("how are you? (\033[1mg\033[22mood, \033[1mf\033[22mine)\n"),
+		scripter.Expect("how are you? (\033[1mg\033[22mood, \033[1mf\033[22mine) [good]\n"),
 		scripter.Reply("f\n"),
 	)
 
@@ -46,7 +49,7 @@ func TestPromptReturnsValidAnswer_FullName(t *testing.T) {
 
 	script := scripter.NewScript(
 		t,
-		scripter.Expect("how are you? (\033[1mg\033[22mood, \033[1mf\033[22mine)\n"),
+		scripter.Expect("how are you? (\033[1mg\033[22mood, \033[1mf\033[22mine) [good]\n"),
 		scripter.Reply("fine\n"),
 	)
 
@@ -68,7 +71,7 @@ func TestPromptEmphasizesFirstMatchingCharacter(t *testing.T) {
 
 	script := scripter.NewScript(
 		t,
-		scripter.Expect("how are you? (I don't kno\033[1mw\033[22m)\n"),
+		scripter.Expect("how are you? (I don't kno\033[1mw\033[22m) [I don't know]\n"),
 		scripter.Reply("w\n"),
 	)
 
@@ -87,7 +90,7 @@ func TestPromptAddsLeadingCharacterIfNoMatching(t *testing.T) {
 
 	script := scripter.NewScript(
 		t,
-		scripter.Expect("how are you? (\033[1mK\033[22m alright)\n"),
+		scripter.Expect("how are you? (\033[1mK\033[22m alright) [alright]\n"),
 		scripter.Reply("k\n"),
 	)
 
@@ -118,7 +121,7 @@ func TestPromoptWritesAllPromptOptions(t *testing.T) {
 
 	script := scripter.NewScript(
 		t,
-		scripter.Expect("how are you? (\033[1mg\033[22mood, \033[1mf\033[22mine, \033[1mo\033[22mtherwise, \033[1mK\033[22m alright)\n"),
+		scripter.Expect("how are you? (\033[1mg\033[22mood, \033[1mf\033[22mine, \033[1mo\033[22mtherwise, \033[1mK\033[22m alright) [good]\n"),
 		scripter.Reply("fine\n"),
 	)
 
@@ -141,7 +144,7 @@ func TestPromptRetriesOnBadAnswer(t *testing.T) {
 
 	script := scripter.NewScript(
 		t,
-		scripter.Expect("how are you? (\033[1mg\033[22mood, \033[1mf\033[22mine)\n"),
+		scripter.Expect("how are you? (\033[1mg\033[22mood, \033[1mf\033[22mine) [good]\n"),
 		scripter.Reply("not great\n"),
 		scripter.Expect("Sorry, that didn't match any of the prompt options.\n"),
 		scripter.Reply("f\n"),
@@ -152,18 +155,125 @@ func TestPromptRetriesOnBadAnswer(t *testing.T) {
 	script.AssertFinished()
 }
 
-func TestPromptPropagatesWriteError(t *testing.T) {
+type errorWriter struct{}
 
+func (ew errorWriter) Write(p []byte) (int, error) {
+	return 0, fmt.Errorf("An error!")
+}
+
+func TestPromptPropagatesWriteError(t *testing.T) {
+	r := strings.NewReader("some value")
+	w := errorWriter{}
+
+	options := []prompt.Answer{
+		{
+			"hopefully it errors",
+			'h',
+		},
+	}
+
+	answer, err := prompt.Prompt(r, w, "What happens if the write fails?", options)
+
+	blankAnswer := prompt.Answer{}
+	if answer != blankAnswer {
+		t.Error("Should return blank answer.")
+	}
+
+	if err.Error() != "An error!" {
+		t.Error("Should have forwarded error from writer!")
+	}
+}
+
+type errorReader struct{}
+
+func (er errorReader) Read(p []byte) (int, error) {
+	return 0, fmt.Errorf("An error!")
+}
+
+type fakeWriter struct{}
+
+func (fw fakeWriter) Write(p []byte) (int, error) {
+	return len(p), nil
 }
 
 func TestPromptPropagatesReadError(t *testing.T) {
+	r := errorReader{}
+	w := fakeWriter{}
 
+	options := []prompt.Answer{
+		{
+			"hopefully it errors",
+			'h',
+		},
+	}
+
+	answer, err := prompt.Prompt(r, w, "What happens if the read fails?", options)
+
+	blankAnswer := prompt.Answer{}
+	if answer != blankAnswer {
+		t.Error("Should return blank answer.")
+	}
+
+	if err.Error() != "An error!" {
+		t.Error("Should have forwarded error from writer!")
+	}
 }
 
 func TestPromptAcceptsCaseInsensitiveShortCode(t *testing.T) {
+	expectedAnswer := prompt.Answer{"yes", 'y'}
+	options := []prompt.Answer{
+		expectedAnswer,
+		{
+			"no",
+			'n',
+		},
+	}
 
+	script := scripter.NewScript(
+		t,
+		scripter.Expect("are you enjoying 'prompt'? (\033[1my\033[22mes, \033[1mn\033[22mo) [yes]\n"),
+		scripter.Reply("Y\n"),
+	)
+
+	answer, err := prompt.Prompt(script.In(), script.Out(), "are you enjoying 'prompt'?", options)
+
+	if err != nil {
+		t.Error("Unexpected error!")
+	}
+
+	if answer != expectedAnswer {
+		t.Error("Did not return expected answer.")
+	}
+
+	script.AssertFinished()
 }
 
 func TestPromptAcceptsCaseInsensitiveLongAnswer(t *testing.T) {
 
+	expectedAnswer := prompt.Answer{"yes", 'y'}
+	options := []prompt.Answer{
+		expectedAnswer,
+		{
+			"no",
+			'n',
+		},
+	}
+
+	script := scripter.NewScript(
+		t,
+		scripter.Expect("are you enjoying 'prompt'? (\033[1my\033[22mes, \033[1mn\033[22mo) [yes]\n"),
+		scripter.Reply("YeS\n"),
+	)
+
+	answer, err := prompt.Prompt(script.In(), script.Out(), "are you enjoying 'prompt'?", options)
+
+	if err != nil {
+		t.Error("Unexpected error!")
+	}
+
+	if answer != expectedAnswer {
+		t.Error("Did not return expected answer.")
+	}
+
+	script.AssertFinished()
 }
